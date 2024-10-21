975499bd3e56e5482ffe3376c3111a2d64d68c3e
AndrewTsuranov <a.tsuranov@gmail.com>
2024-10-21 17:25:40 +0300

    SGI1 2024/10/21 bootstrap-vue-nuxt

--- warehouse/src/main.js
+++ warehouse/src/main.js
@@ -1,9 +1,11 @@
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import {createBootstrap} from 'bootstrap-vue-next'
 import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 import App from './App.vue'
 import router from './router'
 
@@ -13,6 +15,6 @@ const pinia = createPinia()
 app.use(pinia)
 app.use(router)
 app.use(bootstrap)
-
+app.use(createBootstrap())
 app.mount('#app')
 
--- warehouse/vite.config.js
+++ warehouse/vite.config.js
@@ -1,10 +1,15 @@
 import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
+        Components({
+            resolvers: [BootstrapVueNextResolver()],
+        }),
     ],
     server: {
         host: '0.0.0.0',
