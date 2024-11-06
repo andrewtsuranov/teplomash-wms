fd93e50fb11a1c3e0309ef08a333c1f2d0797a2b
AnTs <a.tsuranov@gmail.com>
2024-11-07 01:56:02 +0300

    MAC 2024/11/07 Palett

--- warehouse/src/views/Home/WMS/Storage/Packing/StoragePackingView.vue
+++ warehouse/src/views/Home/WMS/Storage/Packing/StoragePackingView.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="wms-packing-container">
     <h1>Упаковка:</h1>
-    <div class="grid-container">
-      <div v-for="n in 12" :key="n" class="grid-item">
-        <span class="pallet-name">Паллета ID#{{ n }} </span>
+    <div class="wms-packing-pallet">
+      <div v-for="n in 9" :key="n" class="pallet-item">
+        <span class="pallet-name">
+          <span>Зона: А</span>
+          <span>Дата создания: {{ dataYYYYMMDD }}</span>
+         <span>Паллета №{{ n }}</span>
+
+        </span>
+        <span>Длина паллеты: 800мм</span>
+        <span>Тип: Продукция</span>
+        <span>Изделие: КЭВ-9П2021Е</span>
+        <span>Комментарий: Панель из глянцевой нержавеющей стали</span>
+        <span>Кол-во изделий: 9 шт.</span>
+        <div class="pallet-qrcode" v-html="qrcode"></div>
+        <div class="pallet-status">Статус/Этапы сборки:</div>
       </div>
     </div>
-    <canvas id="canvas"></canvas>
     <div>
       <router-view name="TTM"/>
     </div>
@@ -15,19 +26,28 @@
 <script setup>
 import {onMounted, ref} from 'vue'
 import QRCode from 'qrcode'
-const canvasEl = ref(null)
-// const QRCode = require('qrcode')
-const canvas = document.getElementById('canvas')
+
 onMounted(() => {
-  generateQR(ee)
+  generateQR(text.value)
 })
-const ee = 'asfsafs'
-const generateQR = async ee => {
-  try {
-    canvasEl.value = await QRCode.toDataURL(ee)
-  } catch (err) {
-    console.error(err)
+const text = ref('[А]-[2024101600]-[П]-[800]-[КЭВ-9П2021Е]-[Панель из глянцевой нержавеющей стали]-[9]')
+const qrcode = ref(null)
+const dataYYYYMMDD = ref(new Date().toISOString().slice(0, 10));
+const generateQR = async (data) => {
+  const opts = {
+    errorCorrectionLevel: 'H',
+    quality: 0.3,
+    width: 130,
+    margin: 1,
+    color: {
+      dark: "#000",
+      light: '#EEE'
+    }
   }
+  await QRCode.toString(data, opts, (err, string) => {
+    if (err) throw err
+    qrcode.value = string
+  })
 }
 </script>
 <style scoped>
@@ -35,29 +55,54 @@ const generateQR = async ee => {
   display: grid;
   grid-template-columns: 1fr;
   grid-auto-rows: min-content;
-
 }
 
-.grid-container {
+.wms-packing-pallet {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
   gap: 1rem;
   width: 100%;
   padding: 1rem 0;
-
 }
 
-.grid-item {
+.pallet-item {
+  display: grid;
+  grid-template-areas:
+        "palletID palletID"
+        ". ."
+        "status qrcode";
+  grid-template-columns: 1fr min-content;
+  grid-template-rows: min-content min-content 1fr;
   /*border: 2px solid #4CAF50;*/
   border: 2px solid #E8B53F;
   /*border: 2px solid #B01010;*/
-
   padding: 1rem;
-  border-radius: 0.5rem;
-  min-height: 250px;
+  row-gap: 1rem;
+  border-radius: 1rem;
+  min-height: 400px;
 }
 
 .pallet-name {
-  font-size: 1.2rem;
+  display: grid;
+  grid-area: palletID;
+  /*grid-auto-flow: column;*/
+  grid-template-columns: auto 1fr auto;
+  grid-auto-rows: min-content;
+  font-size: 1.1rem;
+  column-gap: 1rem;
+  justify-items: center;
+  /*overflow: auto;*/
+}
+
+.pallet-qrcode {
+  grid-area: qrcode;
+  align-self: end;
+}
+
+.pallet-status {
+  grid-area: status;
+  padding: .5rem 0;
+  border-top: 1px solid red;
+  font-size: 1.3rem;
 }
 </style>
