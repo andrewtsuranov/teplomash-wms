e07fc545284103c661b41d58aedcd85153096bd5
AnTs <a.tsuranov@gmail.com>
2025-01-09 22:34:00 +0300

    MAC 09/01/2025

--- src/views/Home/About/AboutView.vue
+++ src/views/Home/About/AboutView.vue
@@ -1,21 +1,7 @@
 <template>
-  <div class="container-fluid">
-    <h1 class="my-4">Warehouse Management System</h1>
-    <WarehouseSearch/>
-    <div v-if="storageStore.error" class="alert alert-danger">
-      {{ storageStore.error }}
-    </div>
-    <SearchResults/>
-    <WarehouseTopView/>
-    <RackProfileView/>
+  <div class="">
+    <h1>Here is gonna be About Page</h1>
   </div>
 </template>
 <script setup>
-import {useStorageStore} from "@/stores/HTTP/StorageStore.js";
-
-const storageStore = useStorageStore()
-import WarehouseSearch from "@/components/WMS/Process/REC/map/WarehouseSearch.vue";
-import SearchResults from "@/components/WMS/Process/REC/map/SearchResults.vue";
-import WarehouseTopView from "@/components/WMS/Process/REC/WarehouseTopView.vue";
-import RackProfileView from "@/components/WMS/Process/REC/RackProfileView.vue";
 </script>
