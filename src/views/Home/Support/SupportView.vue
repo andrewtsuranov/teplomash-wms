b8513b820da3decd543fe27516aa7d34b3990ca5
AndrewTsuranov <a.tsuranov@gmail.com>
2025-03-20 20:53:35 +0300

    SGI1 2025/03/20 new component pallet type

--- src/views/Home/Support/SupportView.vue
+++ src/views/Home/Support/SupportView.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <h1>Here is gonna be Support page</h1>
+    <FormCreatePalletType/>
   </div>
 </template>
 <script setup>
+import FormCreatePalletType from "@/components/Forms/FormCreatePalletType.vue";
 </script>
 <style scoped>
 </style>
